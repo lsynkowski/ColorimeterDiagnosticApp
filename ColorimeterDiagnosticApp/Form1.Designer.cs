bce1a526566627b70c813c793d87cdcab83b2f39
Lee Synkowski <lsynkowski@taylortechnologies.local>
2020-09-04 10:43:14 -0400

    add Register for notifications

--- ColorimeterDiagnosticApp/Form1.Designer.cs
+++ ColorimeterDiagnosticApp/Form1.Designer.cs
@@ -28,34 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
             this.SuspendLayout();
             // 
-            // label1
+            // listBox1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(61, 69);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(46, 17);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(43, 53);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(426, 212);
+            this.listBox1.TabIndex = 1;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.label1);
+            this.ClientSize = new System.Drawing.Size(600, 366);
+            this.Controls.Add(this.listBox1);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listBox1;
     }
 }
 
--- ColorimeterDiagnosticApp/Form1.cs
+++ ColorimeterDiagnosticApp/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,25 +13,158 @@ namespace ColorimeterDiagnosticApp
 {
     public partial class Form1 : Form
     {
-        public Form1()
+        private const int WM_DEVICECHANGE = 0x219;
+        private const int DBT_DEVICEARRIVAL = 0x8000;
+        private const int DBT_DEVICEREMOVECOMPLETE = 0x8004;
+
+		private const Int32 DBT_DEVTYP_DEVICEINTERFACE = 5;
+		private const Int32 DEVICE_NOTIFY_WINDOW_HANDLE = 0;
+
+
+		public Form1()
         {
             InitializeComponent();
         }
 
         protected override void WndProc(ref Message m)
         {
-            //always do the base
             base.WndProc(ref m);
 
-            //Once the colorimeter is connected we don't handle any more messages
-            //unless we want to handle a remove it message
-            //if (!colorimeterConnected && m.Msg == WM_DEVICECHANGE)
-            if (m.Msg == DeviceManagement.WM_DEVICECHANGE)
+            if (m.Msg == WM_DEVICECHANGE)
             {
-                if (!checkColorimeterConnectionBackgroundWorker.IsBusy)
-                    checkColorimeterConnectionBackgroundWorker.RunWorkerAsync(m);
+				listBox1.Items.Add($"WParam : { m.WParam }   |LParam : { m.LParam }  |Result : { m.Result }  |MHWnd : { m.HWnd }");
 
             }
+
         }
-    }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+
+			Guid hidGuid = Guid.Empty;
+			HidD_GetHidGuid(ref hidGuid);
+
+			IntPtr deviceNotificationHandle;
+			deviceNotificationHandle = IntPtr.Zero;
+
+			RegisterForDeviceNotifications("", this.Handle, hidGuid,ref deviceNotificationHandle);
+
+		}
+
+
+		///  <summary>
+		///  Requests to receive a notification when a device is attached or removed.
+		///  </summary>
+		///  
+		///  <param name="devicePathName"> handle to a device. </param>
+		///  <param name="formHandle"> handle to the window that will receive device events. </param>
+		///  <param name="classGuid"> device interface GUID. </param>
+		///  <param name="deviceNotificationHandle"> returned device notification handle. </param>
+		///  
+		///  <returns>
+		///  True on success.
+		///  </returns>
+		///  
+		private Boolean RegisterForDeviceNotifications(String devicePathName, IntPtr formHandle, Guid classGuid, ref IntPtr deviceNotificationHandle)
+		{
+			// A DEV_BROADCAST_DEVICEINTERFACE header holds information about the request.
+
+			DEV_BROADCAST_DEVICEINTERFACE devBroadcastDeviceInterface = new DEV_BROADCAST_DEVICEINTERFACE();
+			IntPtr devBroadcastDeviceInterfaceBuffer = IntPtr.Zero;
+			Int32 size = 0;
+
+			try
+			{
+				// Set the parameters in the DEV_BROADCAST_DEVICEINTERFACE structure.
+
+				// Set the size.
+
+				size = Marshal.SizeOf(devBroadcastDeviceInterface);
+				devBroadcastDeviceInterface.dbcc_size = size;
+
+				// Request to receive notifications about a class of devices.
+
+				devBroadcastDeviceInterface.dbcc_devicetype = DBT_DEVTYP_DEVICEINTERFACE;
+
+				devBroadcastDeviceInterface.dbcc_reserved = 0;
+
+				// Specify the interface class to receive notifications about.
+
+				devBroadcastDeviceInterface.dbcc_classguid = classGuid;
+
+				// Allocate memory for the buffer that holds the DEV_BROADCAST_DEVICEINTERFACE structure.
+
+				devBroadcastDeviceInterfaceBuffer = Marshal.AllocHGlobal(size);
+
+				// Copy the DEV_BROADCAST_DEVICEINTERFACE structure to the buffer.
+				// Set fDeleteOld True to prevent memory leaks.
+
+				Marshal.StructureToPtr(devBroadcastDeviceInterface, devBroadcastDeviceInterfaceBuffer, true);
+
+				// ***
+				//  API function
+
+				//  summary
+				//  Request to receive notification messages when a device in an interface class
+				//  is attached or removed.
+
+				//  parameters 
+				//  Handle to the window that will receive device events.
+				//  Pointer to a DEV_BROADCAST_DEVICEINTERFACE to specify the type of 
+				//  device to send notifications for.
+				//  DEVICE_NOTIFY_WINDOW_HANDLE indicates the handle is a window handle.
+
+				//  Returns
+				//  Device notification handle or NULL on failure.
+				// ***
+
+				deviceNotificationHandle = RegisterDeviceNotification(formHandle, devBroadcastDeviceInterfaceBuffer, DEVICE_NOTIFY_WINDOW_HANDLE);
+
+				// Marshal data from the unmanaged block devBroadcastDeviceInterfaceBuffer to
+				// the managed object devBroadcastDeviceInterface
+
+				Marshal.PtrToStructure(devBroadcastDeviceInterfaceBuffer, devBroadcastDeviceInterface);
+
+				if ((deviceNotificationHandle.ToInt32() == IntPtr.Zero.ToInt32()))
+				{
+					return false;
+				}
+				else
+				{
+					return true;
+				}
+			}
+			catch (Exception ex)
+			{
+				throw;
+			}
+			finally
+			{
+				if (devBroadcastDeviceInterfaceBuffer != IntPtr.Zero)
+				{
+					// Free the memory allocated previously by AllocHGlobal.
+
+					Marshal.FreeHGlobal(devBroadcastDeviceInterfaceBuffer);
+				}
+			}
+		}
+
+		[StructLayout(LayoutKind.Sequential)]
+		private class DEV_BROADCAST_DEVICEINTERFACE
+		{
+			internal Int32 dbcc_size;
+			internal Int32 dbcc_devicetype;
+			internal Int32 dbcc_reserved;
+			internal Guid dbcc_classguid;
+			internal Int16 dbcc_name;
+		}
+
+
+		[DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+		internal static extern IntPtr RegisterDeviceNotification(IntPtr hRecipient, IntPtr NotificationFilter, Int32 Flags);
+
+		[DllImport("hid.dll", SetLastError = true)]
+		internal static extern void HidD_GetHidGuid(ref System.Guid HidGuid);
+	}
 }
